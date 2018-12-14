8b5b2f82724eed2b2809846a54210a2fbd5beacb
GoJaeHak <goddkfvlwl@gmail.com>
2018-12-14 14:42:33 +0900

    방어구, 아이템, 제작소재 VO

--- MonsterHunterWorld/VO/Armors.cs
+++ MonsterHunterWorld/VO/Armors.cs
@@ -8,5 +8,53 @@ namespace MonsterHunterWorld.VO
 {
     class Armors
     {
+        private int idx;
+        private int set_Num;
+        private int part_No;
+        private string setImage;
+        private string level;
+        private string part;
+        private string name;
+        private int rare;
+        private string slots;
+        private int defense;
+        private Element resistances;
+        private List<Material> items = new List<Material>();
+        private List<Skills> skills = new List<Skills>();
+
+        public Armors()
+        {
+        }
+
+        public Armors(int idx, int set_Num, int part_No, string setImage, string level, string part, string name, int rare, string slots, int defense, Element resistances, List<Material> items, List<Skills> skills)
+        {
+            this.idx = idx;
+            this.set_Num = set_Num;
+            this.part_No = part_No;
+            this.setImage = setImage;
+            this.level = level;
+            this.part = part;
+            this.name = name;
+            this.rare = rare;
+            this.slots = slots;
+            this.defense = defense;
+            this.resistances = resistances;
+            this.items = items;
+            this.skills = skills;
+        }
+
+        public int Idx { get => idx; set => idx = value; }
+        public int Set_Num { get => set_Num; set => set_Num = value; }
+        public int Part_No { get => part_No; set => part_No = value; }
+        public string SetImage { get => setImage; set => setImage = value; }
+        public string Level { get => level; set => level = value; }
+        public string Part { get => part; set => part = value; }
+        public string Name { get => name; set => name = value; }
+        public int Rare { get => rare; set => rare = value; }
+        public string Slots { get => slots; set => slots = value; }
+        public int Defense { get => defense; set => defense = value; }
+        public Element Resistances { get => resistances; set => resistances = value; }
+        public List<Material> Items { get => items; set => items = value; }
+        public List<Skills> Skills { get => skills; set => skills = value; }
     }
 }
