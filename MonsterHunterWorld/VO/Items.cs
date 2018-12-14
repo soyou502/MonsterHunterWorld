ff9f497e9a73650de1838219a7d818763bfbe889
IcJsh <soyou501@gmail.com>
2018-12-14 15:04:01 +0900

    수정완료

--- MonsterHunterWorld/VO/Items.cs
+++ MonsterHunterWorld/VO/Items.cs
@@ -8,5 +8,31 @@ namespace MonsterHunterWorld.VO
 {
     class Items
     {
+        private int idx;
+        private string type;
+        private string name;
+        private string decription;
+        private int rare;
+        private int price;
+        public Items()
+        {
+
+        }
+        public Items(int idx, string type, string name, string decription, int rare, int price)
+        {
+            this.idx = idx;
+            this.type = type;
+            this.name = name;
+            this.decription = decription;
+            this.rare = rare;
+            this.price = price;
+        }
+
+        public int Idx { get => idx; set => idx = value; }
+        public string Type { get => type; set => type = value; }
+        public string Name { get => name; set => name = value; }
+        public string Decription { get => decription; set => decription = value; }
+        public int Rare { get => rare; set => rare = value; }
+        public int Price { get => price; set => price = value; }
     }
 }
---
+++ MonsterHunterWorld/VO/Material.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterHunterWorld.VO
+{
+    class Material
+    {
+        private string name;
+        private int count;
+
+        public Material(string name, int count)
+        {
+            this.name = name;
+            this.count = count;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Count { get => count; set => count = value; }
+    }
+}
