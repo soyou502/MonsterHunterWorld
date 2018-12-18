663259a4ac8ace4b4856187c5446ab541565ad5e
GoJaeHak <goddkfvlwl@gmail.com>
2018-12-18 13:33:09 +0900

    회원가입, 로그인

---
+++ MonsterHunterWorld/DAO/MonsterHunterUserDB.cs
@@ -0,0 +1,65 @@
+﻿using MonsterHunterWorld.VO;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterHunterWorld.DAO
+{
+    class MonsterHunterUserDB
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        public MonsterHunterUserDB()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            con = new SqlConnection();
+            cmd = new SqlCommand();
+            con.ConnectionString = connectionString;
+            cmd.Connection = con;
+        }
+
+        public bool UserIDCheck(string userID)
+        {
+            cmd.CommandText = "select UserId from MonsterHunterUserInfo where UserID = " + userID;
+            con.Open();
+            var check = cmd.ExecuteScalar();
+            con.Close();
+            if (check != null)
+            {
+                return false;
+            }else
+            {
+                return true;
+            }
+        }
+
+        public void InsertUserInfo(string userId, string UserPassword)
+        {
+            cmd.CommandText = "insert into MonsterHunterUserInfo values(" + userId + " ," + UserPassword + ");";
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            System.Windows.Forms.MessageBox.Show("회원가입 성공");
+        }
+
+        public bool UserCheck(string userId, string userPassword)
+        {
+            cmd.CommandText = "select UserId from MonsterHunterUserInfo where UserID = " + userId + " AND UserPassword = " + userPassword;
+            con.Open();
+            var check = cmd.ExecuteScalar();
+            con.Close();
+            if (check != null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ MonsterHunterWorld/FrmLogin.cs
@@ -0,0 +1,43 @@
+﻿using MonsterHunterWorld.BUS;
+using MonsterHunterWorld.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonsterHunterWorld
+{
+    public partial class FrmLogin : Form
+    {
+        MonsterHunterUserDB db;
+        public FrmLogin()
+        {
+            db = new MonsterHunterUserDB();
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FrmsignIn fsi = new FrmsignIn();
+            fsi.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(db.UserCheck(txtUserID.Text, txtUserPassword.Text))
+            {
+                Form1 form = new Form1();
+                form.Show();
+            }else
+            {
+                MessageBox.Show("아이디 또는 비밀번호를 확인해주세요.");
+                return;
+            }
+        }
+    }
+}
