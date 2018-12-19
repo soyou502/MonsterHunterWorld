ba2d507df90a22d4b631d49c08eb7e8c43859953
xodn4098 <xodn4794@gmail.com>
2018-12-19 20:58:04 +0900

    무기폼 업데이트 1. 무기전용 데이터 클래스 추가 ( 무기기본정보(base), 속성(elemental), 상태이상(debuff), 생산및강화정보(create), 예리도(durability) )

--- MonsterHunterWorld/VO/WeaponCreate.cs
+++ MonsterHunterWorld/VO/WeaponCreate.cs
@@ -5,19 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MonsterHunterWorld.VO
-{
+{//
     class WeaponCreate
     {
-        private int idx; // 무기번호
+        private string weapon_name; // 무기번호
         private string imgUri; // 이미지
         private bool con_make; // 제작가능여부
         private string derivation; // 소재파생
         private int make_price; // 제작비용
         private int upgrade_price; // 업글비용
 
-        public WeaponCreate(int idx, string imgUri, bool con_make, int make_price, int upgrade_price, string derivation)
+        public WeaponCreate(string weapon_name, string imgUri, bool con_make, int make_price, int upgrade_price, string derivation)
         {
-            Idx = idx;
+            Weapon_name = weapon_name;
             ImgUri = imgUri;
             Con_make = con_make;
             Make_price = make_price;
@@ -25,7 +25,7 @@ namespace MonsterHunterWorld.VO
             Derivation = derivation;
         }
 
-        public int Idx { get => idx; set => idx = value; }
+        public string Weapon_name { get => weapon_name; set => weapon_name = value; }
         public string ImgUri { get => imgUri; set => imgUri = value; }
         public bool Con_make { get => con_make; set => con_make = value; }
         public int Make_price { get => make_price; set => make_price = value; }
--- MonsterHunterWorld/VO/WeaponEelemental.cs
+++ MonsterHunterWorld/VO/WeaponEelemental.cs
@@ -5,28 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MonsterHunterWorld.VO
-{
+{//
     class WeaponEelemental
     {
-        private int idx; // 무기번호
         private string elmental_name; // 속성 이름
         private int elmental_value; // 속성 수치
-        private string debuff_type; // 상태이상 이름
-        private int debuff_value; // 상태이상 수치
+        private int idx; // 무기번호
 
-        public WeaponEelemental(int idx, string elmental_name, int elmental_value, string debuff_type, int debuff_value)
+        public WeaponEelemental(int idx, string elmental_name, int elmental_value)
         {
             Idx = idx;
             Elmental_name = elmental_name;
             Elmental_value = elmental_value;
-            Debuff_type = debuff_type;
-            Debuff_value = debuff_value;
         }
 
         public int Idx { get => idx; set => idx = value; }
         public string Elmental_name { get => elmental_name; set => elmental_name = value; }
         public int Elmental_value { get => elmental_value; set => elmental_value = value; }
-        public string Debuff_type { get => debuff_type; set => debuff_type = value; }
-        public int Debuff_value { get => debuff_value; set => debuff_value = value; }
+
     }
 }
