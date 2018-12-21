f62b309d1cfa6fd2432c97b0dfc8f0db523c87bc
xodn4098 <xodn4794@gmail.com>
2018-12-21 13:08:17 +0900

    무기 정보 데이터 세분화 ( 기본정보, 이미지, 예리도 , 속성 ,상태이상, 제작정보 )

--- MonsterHunterWorld/VO/WeaponBase.cs
+++ MonsterHunterWorld/VO/WeaponBase.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MonsterHunterWorld.VO
-{
+{//
     class WeaponBase
     {
         // 기본 정보
---
+++ MonsterHunterWorld/VO/WeaponDebuff.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterHunterWorld.VO
+{//
+    class WeaponDebuff
+    {
+        private int idx; // 무기번호
+        private string debuff_type; // 상태이상 이름
+        private int debuff_value; // 상태이상 수치
+
+        public WeaponDebuff(int idx, string debuff_type, int debuff_value)
+        {
+            Debuff_type = debuff_type;
+            Debuff_value = debuff_value;
+            Idx = idx;
+        }
+
+        public string Debuff_type { get => debuff_type; set => debuff_type = value; }
+        public int Debuff_value { get => debuff_value; set => debuff_value = value; }
+        public int Idx { get => idx; set => idx = value; }
+    }
+}
